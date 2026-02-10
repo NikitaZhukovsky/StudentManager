b96a361d96f9e9dea2cf11f56900f1de2eb87643
NikitaZhukovsky <nikzuk2004@mail.ru>
2026-02-10 11:13:22 +0300

    Add students views

---
+++ students/schemas.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from ninja import Schema
+from pydantic import EmailStr
+
+
+class StudentCreateSchema(Schema):
+    full_name: str
+    email: EmailStr
+
+
+class StudentUpdateSchema(Schema):
+    full_name: Optional[str] = None
+    email: Optional[EmailStr] = None
+
+
+class StudentGetSchema(Schema):
+    id: int
+    full_name: str
+    email: str
+
+
+class StudentFilterSchema(Schema):
+    search: Optional[str] = None
+    order_by: Optional[str] = "id"
+    order: Optional[str] = "asc"
+
